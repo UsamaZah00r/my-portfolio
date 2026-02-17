1dae8a4316d95dd37e1c91fe8b7af3ef336c0544
UsamaZah00r <usamazahoorchadhar54@gmail.com>
2026-02-17 22:25:22 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -8,44 +8,21 @@ import Projects from './components/Projects'
 import Education from './components/Education'
 import Contact from './components/Contact'
 import BackgroundAnimation from './components/BackgroundAnimation'
-import LoadingScreen from './components/LoadingScreen'
-import ScrollProgress from './components/ScrollProgress'
-import Testimonials from './components/Testimonials'
-import Blog from './components/Blog'
-import CodeShowcase from './components/CodeShowcase'
-import PerformanceMetrics from './components/PerformanceMetrics'
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
-
-  const handleLoadingComplete = () => {
-    setIsLoading(false)
-  }
-
   return (
     <div className="relative min-h-screen bg-slate-950 text-white">
-      {isLoading && <LoadingScreen onLoadingComplete={handleLoadingComplete} />}
-      
-      {!isLoading && (
-        <>
-          <ScrollProgress />
-          <BackgroundAnimation />
-          <Header />
-          <main className="relative z-10">
-            <Hero />
-            <About />
-            <Skills />
-            <Experience />
-            <Projects />
-            <CodeShowcase />
-            <PerformanceMetrics />
-            <Education />
-            <Testimonials />
-            <Blog />
-            <Contact />
-          </main>
-        </>
-      )}
+      <BackgroundAnimation />
+      <Header />
+      <main className="relative z-10">
+        <Hero />
+        <About />
+        <Skills />
+        <Experience />
+        <Projects />
+        <Education />
+        <Contact />
+      </main>
     </div>
   )
 }
